aba2f9bfa5ac36e66773ae43393100c449f54d3f
EJKim <dmdwn3979@gmail.com>
2022-07-16 21:47:26 +0900

    feat: 사용자 컴포넌트 기능 구현

--- renderer/component/Users/index.tsx
+++ renderer/component/Users/index.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getUserOnline } from '../../common/firebase/userStatus'
+import { Avatar, List } from 'antd';
+import { startChat } from '../../common/firebase/chat'
+interface UserList {
+    uid: string,
+    displayName: string,
+}
+
 function Users() {
-    console.log(getUserOnline())
+    const [userList, setUserList] = useState<UserList[]>([]);
+    
+    useEffect(() => {
+        async function fetchUserData(){
+            const data = await getUserOnline();
+            console.log(data);
+            setUserList(data);
+        };
+        fetchUserData();
+    },[])
+
+    const startChatWith = (opponentUID) => {
+        startChat(opponentUID);
+    }
+
     return (
     <React.Fragment>
-        users
+        <List
+            itemLayout="horizontal"
+            dataSource={userList}
+            renderItem={user => (
+            <List.Item>
+                <List.Item.Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title={<a onClick={()=>startChatWith(user.uid)}>{user.displayName}</a>}
+                />
+            </List.Item>
+            )}
+        />
     </React.Fragment>
     );
 };
 
 export default Users;
+
+
