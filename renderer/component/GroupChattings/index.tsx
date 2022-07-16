2016fcfbd42178b5f4ab67cd381a7304f96bdfcd
EJKim <dmdwn3979@gmail.com>
2022-07-16 21:47:10 +0900

    feat: 그룹 채팅 컴포넌트 기능 구현

--- renderer/component/GroupChattings/index.tsx
+++ renderer/component/GroupChattings/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Avatar, List } from 'antd';
+import { getGroupChatRooms, startGroupChat } from '../../common/firebase/chat'
+import GroupChattingRoom from './GroupChattingRoom';
 
+interface GroupChat {
+    displayName: string,
+    lastChatUpdate: string,
+    members: string[],
+    uid: string,
+}
 function GroupChattings() {
+    const [chatList, setChatList] = useState<GroupChat[]>([]);
+    const [roomState, setRoomState] = useState<boolean>(false);
+    const [roomInfo, setRoomInfo] = useState<GroupChat>();
+    useEffect(() => {
+        async function fetchChatData(){
+            getGroupChatRooms().then((res)=>{
+                console.log(res);
+                for(let key in res){
+                    console.log(res[key])
+                    setChatList(prev=>[...prev, Object.assign(res[key], {uid: key})])
+                }
+                
+            })
+            
+        };
+        fetchChatData();
+    },[])
+
+    useEffect(()=>{
+        console.log(chatList)
+    },[chatList])
+
+    const enterGroupChat = (roomInfo) => {
+        return (<GroupChattingRoom roomInfo={roomInfo}/>)
+    }
     return (
-    <React.Fragment>
-        GroupChattings
-    </React.Fragment>
+        <React.Fragment>
+            {roomState && enterGroupChat(roomInfo)}
+            {!roomState && <List
+                itemLayout="horizontal"
+                dataSource={chatList}
+                renderItem={chat => {
+
+                    return(
+                        <List.Item>
+                            <List.Item.Meta
+                            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                            title={<a onClick={()=>{
+                                setRoomInfo(chat);
+                                setRoomState(true)
+                            }}>{chat.displayName}</a>}
+                            />
+                        </List.Item>
+                )}}
+            />}
+        </React.Fragment>
     );
 };
 
