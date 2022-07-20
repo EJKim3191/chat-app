d23b29cc18af1b22f86dd6fd00d308476a737d28
EJKim <dmdwn3979@gmail.com>
2022-07-20 22:21:04 +0900

    add: firebase config 추가

--- renderer/common/firebase/firebaseInstance.js
+++ renderer/common/firebase/firebaseInstance.js
@@ -6,8 +6,6 @@ import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_APP_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
